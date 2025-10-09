38cdf7579bfebf20b8036eac1d6a9fb60d52a514
Swashata Ghosh <swas@freemius.com>
2025-10-09 15:56:02 +0530

    [checkout] Implement the paywall and checkout

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -3,15 +3,24 @@ import { auth } from '@/lib/auth';
 import { headers } from 'next/headers';
 import { examples } from '@/lib/ai';
 import AiApp from './ai-app';
+import { freemius } from '@/lib/freemius';
+import AppCheckoutProvider from '@/components/app-checkout-provider';
 
 export default async function Dashboard() {
     const session = await auth.api.getSession({
         headers: await headers(),
     });
 
+    const checkout = await freemius.checkout.create({
+        user: session?.user,
+        isSandbox: process.env.NODE_ENV !== 'production',
+    });
+
     return (
         <AppMain title="New Chat" isLoggedIn={!!session}>
-            <AiApp examples={examples} />
+            <AppCheckoutProvider checkout={checkout.serialize()}>
+                <AiApp examples={examples} />
+            </AppCheckoutProvider>
         </AppMain>
     );
 }

58cc11a016cfea945597c56c7a7ff96b3aff443e
Swashata Ghosh <swas@freemius.com>
2025-10-09 13:24:18 +0530

    [credit] Fix credit page formatting function

--- src/app/credits/credits.tsx
+++ src/app/credits/credits.tsx
@@ -2,14 +2,15 @@
 
 import { Subscribe } from '@/react-starter/components/subscribe';
 import { Topup } from '@/react-starter/components/topup';
-import { formatNumber } from '@/react-starter/utils/formatter';
 
 export default function Credits(props: { credits?: number; hasSubscription?: boolean }) {
     const { credits, hasSubscription } = props;
 
+    const formattedCredit = new Intl.NumberFormat().format(credits ?? 0);
+
     const creditUi = (
         <div className="text-center">
-            <h2 className="text-lg font-medium">You have {formatNumber(credits ?? 0)} credits</h2>
+            <h2 className="text-lg font-medium">You have {formattedCredit} credits</h2>
             <p className="mb-10 text-muted-foreground">You can purchase more credits below.</p>
         </div>
     );
